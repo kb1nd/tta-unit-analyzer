b8c4f8d871aac78dd164075641d86ae96a0b6a4a
Keybinder <cutygg06@gmail.com>
2024-12-31 19:24:47 +0100

    js

---
+++ server.js
@@ -0,0 +1,20 @@
+const dep = {
+  fs: import("fs"),
+  http: import("http"),
+  ejs: import("ejs"),
+};
+const port = process.env.PORT || 3001;
+async function handle(req, res) {
+  let header;
+  dep.fs.readFileSync("src/index.ejs", { encoding: "utf8" }, (data) => {
+    header = {
+      type: "text/html",
+      body: dep.ejs.render(data, { test: "hello world" }, { async: true }),
+    };
+    res.writeHeader(200, { "Content-Type": header.type });
+    res.write(header.body);
+    res.end();
+  });
+}
+const server = dep.http.createServer((req, res) => handle(req, res));
+server.listen(port);
--- src/script.js
+++ src/script.js
@@ -11,17 +11,6 @@ function launchPopup(type, data) {
   }
 }
 
-function roundupBuffcd(true_cd, buff_cd) {
-  switch (true) {
-    case Math.round(true_cd / buff_cd) < 2:
-      while (i < buff_cd) i += true_cd;
-      return i;
-      break;
-    default:
-      return true_cd * Math.round(true_cd / buff_cd);
-      break;
-  }
-}
 function buffDps(true_cd, buff_cd, buff_dmg) {
   let buff_dps;
   switch (true) {
@@ -29,7 +18,8 @@ function buffDps(true_cd, buff_cd, buff_dmg) {
       buff_dps = buff_dmg / true_cd;
       break;
     case true_cd < buff_cd:
-      buff_dps = buff_dmg / roundupBuffcd(true_cd, buff_cd);
+      while (i < buff_cd) i += true_cd;
+      buff_dps = buff_dmg / i;
       break;
     case true_cd === buff_cd:
       buff_dps = buff_dmg / buff_cd;
