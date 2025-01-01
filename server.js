b54b382319e7af34f85fe7095949408ed6b1e90c
Keybinder <cutygg06@gmail.com>
2025-01-01 15:05:39 +0100

    js

--- server.js
+++ server.js
@@ -1,20 +1,16 @@
 const dep = { fs: require("fs"), http: require("http"), ejs: require("ejs") };
 const port = process.env.PORT || 3001;
 const server = dep.http.createServer(function (req, res) {
-  dep.ejs.renderFile("index.ejs", {}, {}, function (err, str) {
+  dep.ejs.renderFile("view/index.ejs", {}, {}, function (err, str) {
     if (err) {
-      console.log("Error when parsing EJS:", err);
+      console.log("Error when parsing EJS: ", err);
       res.writeHead(500, { "Content-Type": "text/plain" });
       res.write("Internal Server Error");
       res.end();
       return;
     }
-    const header = {
-      type: "text/html",
-      body: str,
-    };
-    res.writeHead(200, { "Content-Type": header.type });
-    res.write(header.body);
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write(str);
     res.end();
   });
 });
