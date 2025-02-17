168be81dc073db3cb017722f36b3b9d3323e7c4d
Keybinder <cutygg06@gmail.com>
2025-02-17 19:11:40 +0100

    walk

--- server.js
+++ server.js
@@ -1,19 +1,73 @@
-const dep = { fs: require("fs"), http: require("http"), ejs: require("ejs") };
+const fs = require("fs");
+const http = require("http");
+const WebSocket = require("ws");
 const port = process.env.PORT || 3001;
-const server = dep.http.createServer(function (req, res) {
-  dep.ejs.renderFile("view/index.ejs", {}, {}, function (err, str) {
-    if (err) {
-      console.log("Error when parsing EJS: ", err);
-      res.writeHead(500, { "Content-Type": "text/plain" });
-      res.write("Internal Server Error");
-      res.end();
-      return;
-    }
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.write(str);
-    res.end();
+const Cache = [];
+const Eval = (key) => {
+  return Cache.find((stream) => stream.key === key) ? true : false;
+};
+const Post = (key, payload) =>
+  (Cache[Cache.indexOf(Cache.find((stream) => stream.key === key))] = {
+    key: key,
+    data: payload,
   });
+const Create = (key, payload) =>
+  (Cache[Cache.length] = {
+    key: key,
+    data: payload,
+  });
+const getRoute = (route) =>
+  fs.readFile(__dirname + route).then((html) => {
+    return html;
+  });
+const server = http.createServer(function (req, res) {
+  res.setHeader("Content-Type", "text/html");
+  res.writeHead(200);
+  switch (req.url) {
+    case "/docker":
+      res.end(getRoute("views/docker.html"));
+      break;
+    case "/playground":
+      res.end(getRoute("views/playground.html"));
+      break;
+    case "/calculator":
+      res.end(getRoute("views/calculator.html"));
+      break;
+    default:
+      res.end(getRoute("views/index.html"));
+      break;
+  }
 });
+const wss = new WebSocket.Server(server);
 server.listen(port, "0.0.0.0", () => {
   console.log(`Server is running on http://0.0.0.0:${port}`);
+});
+wss.on("connection", (ws) => {
+  ws.on("message", (json) => {
+    let data = JSON.parse(json);
+    switch (Eval(data.key)) {
+      case true:
+        switch (data.protocol) {
+          case "post":
+            Post(data.key, data.payload);
+            break;
+          case "get":
+            ws.send(
+              Cache[Cache.indexOf(Cache.find((stream) => stream.key === key))]
+                .data,
+            );
+            break;
+        }
+        break;
+      default:
+        if (data.protocol === "establish")
+          try {
+            Create(data.payload.key, data.payload.stream);
+          } catch (error) {
+            console.log(error);
+            return;
+          }
+        break;
+    }
+  });
 });
